47bf9f0f59ae57edc5ceb4f5cfb232b734ad7a68
Phuc <119803257+pauln17@users.noreply.github.com>
2026-02-14 23:58:33 -0500

    user endpoint

--- apps/server/src/controllers/user.ts
+++ apps/server/src/controllers/user.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from "express";
+import { prisma } from "src/lib/prisma";
 
-const lookupUserByEmail = async (req: Request, res: Response) => {};
+const lookupUserByEmail = async (req: Request, res: Response) => {
+    const email = req.params.email;
+    if (!email || typeof email !== "string") return res.status(400).json({ error: "Email Required" });
+    try {
+        const user = await prisma.user.findUnique({ where: { email } });
+        return res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+};
 
 export { lookupUserByEmail };
