49a3674cfa60f2bef5b2bc98abf536627df5464b
Phuc <119803257+pauln17@users.noreply.github.com>
2026-02-14 19:34:32 -0500

    finish column endpoint

--- apps/server/src/controllers/table-column.ts
+++ apps/server/src/controllers/table-column.ts
@@ -1,38 +1,62 @@
 import { Request, Response } from "express";
 import { prisma } from "../lib/prisma";
 
-const createColumn = async (req: Request, res: Response) => {
-    const schemaTableId = req.body.schemaTableId;
-    if (!schemaTableId || typeof schemaTableId !== "string") return res.status(400).json({ error: "Schema Table ID Required" });
-
-    const { name, dataType, order } = req.body;
-
-    if (!name) return res.status(400).json({ error: "Name Required" });
-    if (!dataType) return res.status(400).json({ error: "Data Type Required" });
-    if (order === undefined || order === null) return res.status(400).json({ error: "Order Required" });
-
+const createColumnRelation = async (req: Request, res: Response) => {
+    const { sourceColumnId, targetColumnId } = req.body;
+    if (!sourceColumnId || typeof sourceColumnId !== "string") return res.status(400).json({ error: "Source Column ID Required" });
+    if (!targetColumnId || typeof targetColumnId !== "string") return res.status(400).json({ error: "Target Column ID Required" });
     try {
-        const table = await prisma.schemaTable.findFirst({ where: { id: schemaTableId, schemaId: req.schema!.id } });
-        if (!table) return res.status(404).json({ error: "Schema Table Not Found" });
+        const sourceColumn = await prisma.tableColumn.findFirst({ where: { id: sourceColumnId, schemaTableId: req.schema!.id } });
+        if (!sourceColumn) return res.status(404).json({ error: "Source Column Not Found" });
+        const targetColumn = await prisma.tableColumn.findFirst({ where: { id: targetColumnId, schemaTableId: req.schema!.id } });
+        if (!targetColumn) return res.status(404).json({ error: "Target Column Not Found" });
 
-        const column = await prisma.tableColumn.create({
-            data: { name, dataType, order: Number(order), schemaTableId } as { name: string; dataType: string; order: number; schemaTableId: string },
+        const { onDelete, onUpdate } = req.body;
+        const data = Object.fromEntries(Object.entries({ onDelete, onUpdate }).filter(([_, value]) => value !== undefined));
+
+        const columnRelation = await prisma.columnRelation.create({
+            data: { sourceColumnId, targetColumnId, ...data },
         });
 
-        await prisma.schemaTable.update({
-            where: { id: schemaTableId },
-            data: { tableColumns: { connect: { id: column.id } } },
+        await prisma.tableColumn.update({
+            where: { id: sourceColumnId },
+            data: { outgoingReferences: { connect: { id: columnRelation.id } } },
         });
-
-        return res.status(201).json(column);
+        await prisma.tableColumn.update({
+            where: { id: targetColumnId },
+            data: { incomingReferences: { connect: { id: columnRelation.id } } },
+        });
+        return res.status(201).json(columnRelation);
     } catch (error) {
         console.error(error);
         return res.status(500).json({ error: "Internal Server Error" });
     }
 };
 
-const updateColumn = async (req: Request, res: Response) => { };
+const updateColumnRelation = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    if (!id || typeof id !== "string") return res.status(400).json({ error: "Column Relation ID Required" });
+    const { onDelete, onUpdate } = req.body;
+    const data = Object.fromEntries(Object.entries({ onDelete, onUpdate }).filter(([_, value]) => value !== undefined));
+    try {
+        const columnRelation = await prisma.columnRelation.update({ where: { id }, data });
+        return res.status(200).json(columnRelation);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+};
 
-const deleteColumn = async (req: Request, res: Response) => { };
+const deleteColumnRelation = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    if (!id || typeof id !== "string") return res.status(400).json({ error: "Column Relation ID Required" });
+    try {
+        const columnRelation = await prisma.columnRelation.delete({ where: { id } });
+        return res.status(200).json(columnRelation);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+};
 
-export { createColumn, updateColumn, deleteColumn };
+export { createColumnRelation, updateColumnRelation, deleteColumnRelation };
