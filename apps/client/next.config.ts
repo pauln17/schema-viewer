44b2094fb6726ff01bfc4d25bf466b2ed4c88b9d
Phuc <119803257+pauln17@users.noreply.github.com>
2026-03-06 11:32:37 -0500

    no-referrer policy for security

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
